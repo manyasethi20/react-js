0fbc9607ded6f8bae0016ec480a7a1242f6ce149
Manya Sethi <manyasethi20@gmail.com>
2025-02-23 18:38:40 +0530

    basic react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>React | Manya Sethi - learnt various things in basic react folder. Created it using npx create-react-app 01basicreact, then in package.json learnt about scripts like start and build which I have used till now, development for which browser it is suitable like chrome, firefox, safari , web-vitals, react and react-dom. In src two main files app.js and index.js , first in app.js made a header tag and in index.js checked if its working or not.</h1>
+  );
+}
+
+export default App;
