93d3b9b38e975cf84bf884fa165c3070f2f8915f
Manya Sethi <manyasethi20@gmail.com>
2025-02-23 18:39:21 +0530

    react with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>React with vite | Manya Sethi - Learnt about react with vite. Created it using npm create vite@latest. In package.json there are only two dependencies i.e. react and react-dom, in script used dev by writing npm run dev. There two main files in src folder i.e. App.jsx and main.jsx (deleted all other files like the css ones). In App.jsx made the header and removed all the packages which are not currently in use.</h1>
+  )
+}
+
+export default App
